8ffe8893f8c8329e61853286caa57feb7fa6fefa
cristovaobcc <cristovao.bcc@gmail.com>
2021-04-30 10:38:41 -0300

    Testa de novo App.main() para criar uma janela vazia.
    Note que o console mostrará um erro devido a ausência de arquivo css.

--- src/main/java/br/com/crudhibernate/application/App.java
+++ src/main/java/br/com/crudhibernate/application/App.java
@@ -13,32 +13,18 @@ public class App extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-//		try {
-//			BorderPane root = new BorderPane();
-//			Scene scene = new Scene(root, 400, 400);
-//			scene.getStylesheets().add(getClass().getResource("").toExternalForm());
-//			primaryStage.setScene(scene);
-//			primaryStage.show();
-//			
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root, 400, 400);
+			scene.getStylesheets().add(getClass().getResource("").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
-		primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
+		
 
 	}
 
