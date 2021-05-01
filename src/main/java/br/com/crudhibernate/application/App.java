8a318239e5f42477f34aafb034f32975f5726f2f
cristovaobcc <cristovao.bcc@gmail.com>
2021-05-01 12:13:16 -0300

    Teste de carregamento da View.fxml c/ App.java
    nota: o Eclipse continua reclamando de alguma coisa no projeto a essa altura do desenvolvimento. Ainda não descobri o por quê. O 'x' vermelho só aparece na pasta razi do projeto.

--- src/main/java/br/com/crudhibernate/application/App.java
+++ src/main/java/br/com/crudhibernate/application/App.java
@@ -3,23 +3,29 @@ package br.com.crudhibernate.application;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root, 400, 400);
-			scene.getStylesheets().add(getClass().getResource("").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/br/com/crudhibernate/view/View.fxml"));
 			
+			
+			mainScene = new Scene(loader.load());
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Crud Hibernate c/ JPA");
+			primaryStage.show();
+		
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
